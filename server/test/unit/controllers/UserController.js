a36da1ffcdb37df6532dae93088c1acd1e6d16f6
GHMarques <gustavo.marques2011@gmail.com>
2018-05-01 11:52:17 -0300

    Test unit: user

--- server/test/unit/controllers/UserController.js
+++ server/test/unit/controllers/UserController.js
@@ -1,35 +1,25 @@
 import UserController from '../../../controllers/UserController';
 
-describe('Controllers: User', () => {
+describe('Controller: User', () => {
   const defaultUser = {
     id: 1,
     name: 'admin',
-    email: 'admin@admin.com',
-    password: '12345',
+    email: 'gustavo.marques2011@gmail.com',
+    password: '123456',
     type: 0,
     validate_professor: 0,
     profile_image_path: 'path',
   };
-  beforeEach((done) => {
-    UserController.User
-      .destroy({ where: {} })
-      .then(() => {
-        UserController.User.create(defaultUser)
-          .then(() => {
-            done();
-          })
-          .catch(err => done(err));
-      })
-      .catch(err => done(err));
-  });
-  describe('Get all users: getAll()', () => {
-    it('should return a list of users', () => UserController.getAll()
-      .then(response => Array.isArray(response)));
-  });
-
-  describe('Get a user: getById()', () => {
-    it('should return a user', () => UserController.getById({ id: 1 })
-      .then(response => expect(response).to.be.eql(defaultUser)));
+  before(async () => {
+    try {
+      await UserController.User.destroy({ where: {} });
+      await UserController.User.create(defaultUser);
+      // done();
+    } catch (err) {
+      /*eslint-disable */
+      console.log(err);
+      /* eslint-enable */
+    }
   });
 
   describe('Create a user: create()', () => {
@@ -44,8 +34,40 @@ describe('Controllers: User', () => {
         profile_image_path: 'path',
       };
 
-      return UserController.create(requestUser)
-        .then(response => expect(response.dataValues).to.be.eql(requestUser));
+      UserController.create(requestUser)
+        .then((response) => {
+          expect(response.status).to.be.eql(200);
+          done();
+        })
+        .catch((err) => {
+          done(`Catch was called: ${err}`);
+        });
+    });
+  });
+
+  describe('Get all users: getAll()', () => {
+    it('should return a list of users', () => {
+      UserController.getAll()
+        .then((response) => {
+          expect(response.status).to.be.eql(2);
+          done();
+        })
+        .catch((err) => {
+          done(`Catch was called: ${err}`);
+        });
+    });
+  });
+
+  describe('Get a user: getById()', () => {
+    it('should return a user', () => {
+      UserController.getById({ id: 1 })
+        .then((response) => {
+          expect(response).to.be.eql(defaultUser);
+          done();
+        })
+        .catch((err) => {
+          done(`Catch was called: ${err}`);
+        });
     });
   });
 
@@ -56,13 +78,91 @@ describe('Controllers: User', () => {
         name: 'Test user updated',
       };
 
-      return UserController.update(requestUser, { id: 1 })
-        .then(response => expect(parseInt(response, 10)).to.be.above(0));
+      UserController.update(requestUser, { id: 1 })
+        .then((response) => {
+          expect(response.status).to.be.eql(200);
+          done();
+        })
+        .catch((err) => {
+          done(`Catch was called: ${err}`);
+        });
     });
   });
 
   describe('Delete a user: delete()', () => {
-    it('should delete a user', () => UserController.delete({ id: 1 })
-      .then(response => expect(parseInt(response, 10)).to.be.above(0)));
+    it('should delete a user', () => {
+      UserController.delete({ id: 2 })
+        .then((response) => {
+          expect(response.status).to.be.eql(200);
+          UserController.getById({id: 2})
+            .then((user) => {
+              expect(user).to.be.eql(null);
+              done();
+            })
+            .catch((err) => {
+              done(`Catch was called: ${err}`);
+            });
+        })
+        .catch((err) => {
+          done(`Catch was called: ${err}`);
+        });
+    });
   });
+
+  describe('Login: signIn()', () => {
+    it('should return a token', () => {
+      UserController.signIn(null, defaultUser, null)
+        .then((response) => {
+          expect(response.result).to.be.eql('Success');
+          done();
+        })
+        .catch((err) => {
+          done(`Catch was called: ${err}`);
+        });
+    });
+  });
+
+  describe('Check token: checkToken()', () => {
+    it('should check the token', () => {
+      UserController.checkToken(defaultUser)
+        .then((response) => {
+          expect(response.result).to.be.eql('Success');
+          done();
+        })
+        .catch((err) => {
+          done(`Catch was called: ${err}`);
+        });
+    });
+  });
+
+  /*describe('Forgot password: forgotPassword()', () => {
+    it('should send an email', () => {
+      UserController.forgotPassword(defaultUser)
+        .then((response) => {
+          expect(response.status).to.be.eql(200);
+          done();
+        })
+        .catch((err) => {
+          done(`Catch was called: ${err}`);
+        });
+    });
+  });*/
+
+  describe('Reset password: resetPassword()', () => {
+    it('should reset the password', () => {
+      let data = {
+        password: '654321',
+        confirmPassword: '654321'
+      }
+      UserController.forgotPassword(data)
+        .then((response) => {
+          expect(response.status).to.be.eql(200);
+          done();
+        })
+        .catch((err) => {
+          done(`Catch was called: ${err}`);
+        });
+    });
+  });
+  
 });
