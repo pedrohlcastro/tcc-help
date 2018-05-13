49002b3280a523337d5f70e5509e41668bb0a8a0
pedro <pedrohlcastro@gmail.com>
2018-05-13 12:14:53 -0300

    Add pdf2txt

--- server/controllers/TccController.js
+++ server/controllers/TccController.js
@@ -1,5 +1,9 @@
 import fs from 'fs';
 import path from 'path';
+import async from 'async';
+import scissors from 'scissors';
+import { spawn } from 'child_process';
+import pdfUtil from 'pdf-to-text';
 /* eslint-disable */
 import Nodehun from 'nodehun';
 /* eslint-enable */
@@ -10,6 +14,38 @@ class TccController {
   constructor() {
     this.Tcc = db().models.Tcc;
   }
+
+  /**
+   * Funcao que trasforma as parada do pdf em txt
+   * @param {string} filePath : endereco absoluto do pdf
+   * @returns {[]sting}: onde array[i] é a pagina i+1, ou seja, array[0] é pagina 1
+   */
+  pdf2Txt(filePath) {
+    return new Promise((resolve, reject) => {
+      const pdf = scissors(filePath);
+      pdf.getNumPages()
+        .then(async (pdfSize) => {
+          const aux = Array.from(Array(parseInt(pdfSize, 10)).keys());
+          const result = new Array(pdfSize);
+          async.forEach(aux, (i, cb) => {
+            pdfUtil.pdfToText(filePath, { from: i, to: i }, (err, data) => {
+              if (err) cb(err);
+              else {
+                result[i] = data;
+                cb();
+              }
+            });
+          }, async (err) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(result);
+            }
+          });
+        });
+    });
+  }
+
   checkSpelling(params) {
     const dictionaryDirectory = path.join(__dirname, '../config/dictionaries');
     const affbuf = fs.readFileSync(`${dictionaryDirectory}/${params.language}.aff`);
--- server/routes/TccRoutes.js
+++ server/routes/TccRoutes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import path from 'path';
 
 import TccController from '../controllers/TccController';
 
@@ -7,6 +8,12 @@ const router = new Router();
 router.get('/spell/:language', (req, res, next) => {
   TccController.checkSpelling(req.params)
     .then(result => res.json(result))
+    .catch(err => next({ err, msg: 'Error running DB query', status: 500 }));
+});
+
+router.get('/parseTxt/:fileName/:page', (req, res, next) => {
+  TccController.dumpPdf2Txt(path.join(__dirname, `../upload/${req.params.fileName}`))
+    .then(data => res.json(data[req.params.page]))
     .catch(err => next({ err, msg: 'Error running DB query', status: 500 }));
 });
 
