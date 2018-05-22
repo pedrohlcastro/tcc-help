dacc4737c4f6b336cbb34c114a638eb4c5920836
Vinícius Henrique Silveira <vhsilveira2@gmail.com>
2018-05-22 00:54:53 -0300

    need to fix router

--- server/routes/RuleRoutes.js
+++ server/routes/RuleRoutes.js
@@ -13,20 +13,11 @@ router.post('/', passport.authenticate('BasicBearer', { session: false }), (req,
     .catch(err => next({ err, msg: 'Error ao criar regra', status: 500 }));
 });
 
-router.get('/', passport.authenticate('BasicBearer', { session: false }), (req, res, next) => {
-  const userId = req.user.id;
-  RuleController.get(userId)
-    .then(data => res.json(data))
-    .catch(err => next({ err, msg: 'Error DB query', status: 500 }));
-});
-
-router.get('/:id', passport.authenticate('BasicBearer', { session: false }), (req, res, next) => {
-  const userId = req.user.id;
-  const ruleId = req.params.id;
-  RuleController.get(userId, { id: ruleId })
-    .then(data => res.json(data))
-    .catch(err => next({ err, msg: 'Error DB query', status: 500 }));
-});
+router.get('/:id', (req, res, next) => {
+  CommentsController.getByStudentId(req.params)
+      .then(data => res.json(data))
+      .catch(err => next({ err, msg: 'Error DB query', status: 500 }));
+  });
 
 router.delete('/:id', passport.authenticate('BasicBearer', { session: false }), (req, res, next) => {
   const ruleId = req.params.id;
